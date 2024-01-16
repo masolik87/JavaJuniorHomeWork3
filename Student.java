975309d3d8fd3d4a39166188df276b5eb7240ba5
dimon <dimon20.07@mail.ru>
2024-01-16 20:26:17 +0300

    .\Student.java

---
+++ Student.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    private int age;
+    private transient double GPA;
+
+    public Student(String name, int age, double GPA) {
+        this.name = name;
+        this.age = age;
+        this.GPA = GPA;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+}
